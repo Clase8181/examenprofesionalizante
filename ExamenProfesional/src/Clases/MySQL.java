be630a3223c55f25bf9483f9b1ff37fefd8b7950
Clase8181 <jbermeo8181@gmail.com>
2017-09-24 22:17:50 -0500

    se realiza una nueva clase denominada matricula para el alumno itsy

---
+++ ExamenProfesional/src/Clases/MySQL.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jaime
+ */
+public class MySQL {
+   public static Connection Conexion;
+ 
+    public void MySQLConnection(String user, String pass, String db_name) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + db_name, user, pass);
+            //System.out.println("Se ha iniciado la conexión con el servidor de forma exitosa");
+            JOptionPane.showMessageDialog(null,"Se ha iniciado la conexión con el servidor de forma exitosa");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+ 
+    public void closeConnection() {
+        try {
+            Conexion.close();
+            System.out.println("Se ha finalizado la conexión con el servidor");
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
+}
